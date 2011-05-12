df8ec8fe8a955dcd1447ebc2724f5777b889550c
John Resig <jeresig@gmail.com>
2011-05-12 17:05:12 -0400

    Make it so that when the videos are clicked it displays the video in the main content area and is playable on the iPad.

--- js/khan.js
+++ js/khan.js
@@ -1,5 +1,10 @@
 var videos;
 
+$(document).delegate( "#playlist a", "mousedown", function() {
+	// Grab the Youtube ID for the video
+	genVideoPage( this.href.substr( this.href.indexOf("#video-") + 7 ) );
+});
+
 $(function() {
 	$.getJSON( "http://www.khanacademy.org/api/playlistvideos?playlist=Algebra&callback=?", function( data ) {
 		videos = data;
@@ -15,7 +20,31 @@ $(function() {
 
 function ready() {
 	$("#playlist")
-		.html( "<li></li>" + tmpl( "video", videos ) )
+		.html( "<li></li>" + tmpl( "videos", videos ) )
 		.listview( "refresh" )
 		.children().first().remove();
+}
+
+function genVideoPage( id ) {
+	var video;
+	
+	// Generated page already exists
+	if ( $("#video-" + id).length ) {
+		return;
+	}
+
+	// Find the associated data blob
+	for ( var i = 0, l = videos.length; i < l; i++ ) {
+		if ( videos[i].youtube_id === id ) {
+			video = videos[i];
+			break;
+		}
+	}
+	
+	// If we found it, add it to the page
+	if ( video ) {
+		$( tmpl( "video", video ) )
+			.appendTo( "#main" )
+			.page();
+	}
 }
