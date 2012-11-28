099109a571c972a464b102e47ce1d1c76adb5eed
Ben Alpert <alpert@khanacademy.org>
2012-11-27 22:53:08 -0800

    Unisubs now returns milliseconds
    Auditors: kamens

--- js/khan.js
+++ js/khan.js
@@ -652,7 +652,7 @@ function showSubtitles( data ) {
 		clearInterval( scrollResume );
 		
 		// Grab the time to jump to from the subtitle
-		pendingSeek = parseFloat( $(e.target).parent().data( "time" ) );
+		pendingSeek = parseFloat( $(e.target).parent().data( "milliseconds" ) ) / 1000;
 		
 		// Start playing the video, if we haven't done so already
 		seekFn = function() {
@@ -676,8 +676,8 @@ function showSubtitles( data ) {
 		// We need to round the number to fix floating point issues
 		var curTime = (pendingSeek || player.currentTime).toFixed(2);
 		
-		for ( var i = 1, l = li.length; i < l; i++ ) {
-			var liTime = $(li[i]).data("time");
+		for ( var i = 0, l = li.length; i < l; i++ ) {
+			var liTime = parseFloat($(li[i]).data("milliseconds")) / 1000;
 			
 			// We're looking for the next highest element before backtracking
 			if ( liTime > curTime && liTime !== curTime ) {
